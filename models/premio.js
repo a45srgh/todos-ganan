6165f964db9a51c74999aff5a0b24297b2958ff9
betuzo <betotsol@gmail.com>
2022-06-29 22:57:58 -0500

    agregando modelo para premios

---
+++ models/premio.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const premioSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 255
+    },
+    costo: {
+        type: Number,
+        required: true,
+        min: 2,
+        max: 255
+    },
+    caracteristicas: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 500
+    },
+    tags: [{
+        type: String
+    }]
+})
+
+module.exports = mongoose.model('Premio', premioSchema);
